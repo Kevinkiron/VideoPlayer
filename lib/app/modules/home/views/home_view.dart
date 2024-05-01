b8059c33b6d8489df39657ac27df998a3ebefa08
Kevin <kvnkiron@gmail.com>
2024-05-02 00:27:30 +0530

    icon build

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -12,41 +12,41 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return YoutubePlayerBuilder(
       builder: (context, player) {
-        return SafeArea(
-          child: Scaffold(
-            appBar: AppBar(
-              title: Style().reg(
-                text: Constants.home,
-              ),
-              centerTitle: true,
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 4,
+            leading: Icon(Icons.video_collection_outlined),
+            title: Style().reg(
+              text: Constants.home,
             ),
-            body: Center(
-                child: ListView(
-              padding: const EdgeInsets.all(10),
-              children: [
-                const Gap(10),
-                Style().reg(
-                  text: Constants.animeEdition,
-                  size: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-                const Gap(10),
-                player,
-                const Gap(20),
-                Style().reg(
-                  text: 'Description',
-                  size: 15,
-                  fontWeight: FontWeight.w600,
-                ),
-                const Gap(10),
-                Style().reg(
-                  text: Constants.descriptionDetail,
-                  size: 15,
-                  fontWeight: FontWeight.w200,
-                ),
-              ],
-            )),
+            centerTitle: true,
           ),
+          body: Center(
+              child: ListView(
+            padding: const EdgeInsets.all(10),
+            children: [
+              const Gap(10),
+              Style().reg(
+                text: Constants.animeEdition,
+                size: 20,
+                fontWeight: FontWeight.w600,
+              ),
+              const Gap(10),
+              player,
+              const Gap(20),
+              Style().reg(
+                text: 'Description',
+                size: 15,
+                fontWeight: FontWeight.w600,
+              ),
+              const Gap(10),
+              Style().reg(
+                text: Constants.descriptionDetail,
+                size: 15,
+                fontWeight: FontWeight.w200,
+              ),
+            ],
+          )),
         );
       },
       player: YoutubePlayer(
