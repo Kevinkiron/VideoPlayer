e2dbbfe7ed1646437904ce3f5240dfd71eb3d926
Kevin <kvnkiron@gmail.com>
2024-05-01 23:40:43 +0530

    splash screen ui complete

--- lib/app/modules/splash_screen/views/splash_screen_view.dart
+++ lib/app/modules/splash_screen/views/splash_screen_view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
 import '../../../utils/app_images.dart';
+import '../../../utils/styles.dart';
 import '../controllers/splash_screen_controller.dart';
 
 class SplashScreenView extends GetView<SplashScreenController> {
@@ -14,7 +15,20 @@ class SplashScreenView extends GetView<SplashScreenController> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-      child: Lottie.asset(AppImages.loading),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Style().reg(
+            text: "CONNECTING",
+            size: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          Lottie.asset(
+            AppImages.loading,
+            fit: BoxFit.fitWidth,
+          ),
+        ],
+      ),
     ));
   }
 }
